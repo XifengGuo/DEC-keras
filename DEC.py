6790d9e9a1aef629393cb7998b6087737fc78966
XifengGuo <guoxifeng1990@163.com>
2019-11-22 10:41:44 +0800

    fix #16

--- DEC.py
+++ DEC.py
@@ -88,7 +88,7 @@ class ClusteringLayer(Layer):
         assert len(input_shape) == 2
         input_dim = input_shape[1]
         self.input_spec = InputSpec(dtype=K.floatx(), shape=(None, input_dim))
-        self.clusters = self.add_weight((self.n_clusters, input_dim), initializer='glorot_uniform', name='clusters')
+        self.clusters = self.add_weight(shape=(self.n_clusters, input_dim), initializer='glorot_uniform', name='clusters')
         if self.initial_weights is not None:
             self.set_weights(self.initial_weights)
             del self.initial_weights
